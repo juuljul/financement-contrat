078867df0c108a4d8373eba92ce60eb60d0c25ee
Julien B <>
2022-11-08 18:48:12 +0100

    unit test withdraw un seul funder

--- backend/test/unit/FinanceContrat.test.js
+++ backend/test/unit/FinanceContrat.test.js
@@ -36,5 +36,35 @@ const { developmentChains } = require("../../helper-hardhat-config")
                 )
                 assert.equal(response.toString(), sendValue.toString())
             })
-        })
+          })
+          describe("withdraw", function () {
+            beforeEach(async () => {
+                await financeContrat.fund({ value: sendValue })
+            })
+            it("withdraw ETH un seul funder", async () => {
+                const startingFinanceContratBalance =
+                    await financeContrat.provider.getBalance(financeContrat.address)
+                const startingDeployerBalance =
+                    await financeContrat.provider.getBalance(deployer)
+
+                const transactionResponse = await financeContrat.withdraw()
+                const transactionReceipt = await transactionResponse.wait()
+                const { gasUsed, effectiveGasPrice } = transactionReceipt
+                const gasCost = gasUsed.mul(effectiveGasPrice)
+
+                const endingFinanceContratBalance = await financeContrat.provider.getBalance(
+                    financeContrat.address
+                )
+                const endingDeployerBalance =
+                    await financeContrat.provider.getBalance(deployer)
+
+                assert.equal(endingFinanceContratBalance, 0)
+                assert.equal(
+                    startingFinanceContratBalance
+                        .add(startingDeployerBalance)
+                        .toString(),
+                    endingDeployerBalance.add(gasCost).toString()
+                )
+            })
+          })
       })
